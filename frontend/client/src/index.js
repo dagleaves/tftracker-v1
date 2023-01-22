1d3ebd3deeccbcf54b64d2827e4e277a9ec8e1f8
Daniel Gleaves <68761152+dagleaves@users.noreply.github.com>
2023-01-22 17:08:06 -0500

    Add firebase import

--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC7k3hs_IMHPHr-YnAbwxlmIUuvtlSh0y0",
+  authDomain: "tftracker-bd209.firebaseapp.com",
+  projectId: "tftracker-bd209",
+  storageBucket: "tftracker-bd209.appspot.com",
+  messagingSenderId: "842684195308",
+  appId: "1:842684195308:web:05854cb076eb6ed2eaa0c9",
+  measurementId: "G-EDHKQXWJD3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
